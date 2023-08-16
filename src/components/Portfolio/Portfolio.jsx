79e8c2be186ce8f3e8fafea23500dec46f4ba126
MICHAELKITH <michaelkithinji27@gmail.com>
2023-08-16 19:00:36 +0300

    Add new image

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -5,7 +5,7 @@ import 'swiper/css';
 import { Pagination } from 'swiper';
 import 'swiper/css/pagination';
 import Sidebar from '../../img/javascrt.png';
-import Budget from '../../img/budget app.png';
+import Budget from '../../img/budget1.png';
 import Todo from '../../img/todo.png';
 import Finance from '../../img/finance app.png';
 import { themeContext } from '../../Context';
@@ -40,10 +40,32 @@ const Portfolio = () => {
       <Swiper modules={[Pagination]} {...swiperParams}>
         <SwiperSlide>
 
+          <img src={Finance} alt="" />
+          <p className="desc">
+            <h1>Finance App</h1>
+            <hr />
+            <h2>Stack: React</h2>
+            React app that displays a list of stocks and services;
+            {'when a user clicks on a stock, the app displays a list of companies '}
+            <br />
+            {' that can be filtered using the search bar'}
+            <br />
+            <div className="btn-live">
+              <a href="https://test-branch--deluxe-puffpuff-cd2d13.netlify.app/">
+                <button className="btn-1" type="button">See Live</button>
+              </a>
+              <a href="https://github.com/MICHAELKITH/Finance_app.git">
+                <button className="btn-2" type="button">Source Code</button>
+              </a>
+            </div>
+          </p>
+        </SwiperSlide>
+        <SwiperSlide>
           <img src={Budget} alt="" />
-
           <p className="desc">
             <h1>Budget App</h1>
+            <hr />
+            <h2>Stack: Ruby On Rails</h2>
             {/* eslint-disable-next-line */}
             {'A budget management app built with Rails and Devise gem with a clean and user-friendly interface'}
             <br />
@@ -58,11 +80,11 @@ const Portfolio = () => {
           </p>
         </SwiperSlide>
         <SwiperSlide>
-
           <img src={Sidebar} alt="" />
-
           <p className="desc">
             <h1>Web Development</h1>
+            <hr />
+            <h2>Stack: HTML, CSS and Javascript</h2>
             {/* eslint-disable-next-line */}
             {' This project was made with JavaScript, HTML, and CSS. The project is responsive to mobile devices'}
             <br />
@@ -80,6 +102,8 @@ const Portfolio = () => {
           <img src={Todo} alt="" />
           <p className="desc">
             <h1>Todo list</h1>
+            <hr />
+            <h2>Stack: HTML, CSS and Javascript</h2>
             Easy, free, and simple-to-use HTML, CSS, and JavaScript
             {' project that may be used to schedule your daily activities'}
             <br />
@@ -88,26 +112,6 @@ const Portfolio = () => {
                 <button className="btn-1" type="button">See Live</button>
               </a>
               <a href="https://github.com/MICHAELKITH/To-do.git">
-                <button className="btn-2" type="button">Source Code</button>
-              </a>
-            </div>
-          </p>
-        </SwiperSlide>
-        <SwiperSlide>
-
-          <img src={Finance} alt="" />
-          <p className="desc">
-            <h1>Finance App</h1>
-            React app that displays a list of stocks and services;
-            {'when a user clicks on a stock, the app displays a list of companies '}
-            <br />
-            {' that can be filtered using the search bar'}
-            <br />
-            <div className="btn-live">
-              <a href="https://test-branch--deluxe-puffpuff-cd2d13.netlify.app/">
-                <button className="btn-1" type="button">See Live</button>
-              </a>
-              <a href="https://github.com/MICHAELKITH/Finance_app.git">
                 <button className="btn-2" type="button">Source Code</button>
               </a>
             </div>
