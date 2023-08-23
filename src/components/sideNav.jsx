12e551d3f9924d1ab9dfa53576df67473e8063f8
MICHAELKITH <michaelkithinji27@gmail.com>
2023-08-23 19:39:23 +0300

    Updated sidenav

--- src/components/sideNav.jsx
+++ src/components/sideNav.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
+import M from 'materialize-css'; // Import Materialize CSS library
 import './sideNav.css';
 
 const myLinks = [
@@ -9,52 +10,43 @@ const myLinks = [
   { to: '/contact', name: 'Contact' },
 ];
 
-let mykey = 'Abcdef1';
-
 const SideNav = () => {
   const sidenavRef = useRef(null);
-  const instanceRef = useRef(null);
 
   useEffect(() => {
-    instanceRef.current = window.M.Sidenav.init(sidenavRef.current);
+    M.Sidenav.init(sidenavRef.current);
   }, []);
 
   const handleNav = () => {
-    if (instanceRef.current && instanceRef.current.isOpen) {
-      instanceRef.current.close();
-    } else if (instanceRef.current) {
-      instanceRef.current.open();
-    }
+    const sidenavInstance = M.Sidenav.getInstance(sidenavRef.current);
+    sidenavInstance.close();
   };
 
   return (
     <>
       <ul id="slide-out" className="sidenav" ref={sidenavRef}>
+        <li><a className="sidenav-close" href="#!" onClick={handleNav}><i className="medium right material-icons"><img src="../images/close.png" alt="LinkedIn" /></i></a></li>
         <li>
           <div className="user-view">
-            {/* <a href="/"><img id="my-profile" src="./images/Profile.jpg" alt="none" /></a> */}
             <p className="black-text name">Michael Kithinji</p>
             <a href="#email"><span className="blue-text email">mikecode945@gmail.com</span></a>
           </div>
         </li>
-        {
-              myLinks.map((mylink) => {
-                mykey += 1;
-                return (
-                  <li key={mykey}>
-                    <NavLink
-                      className={(link) => (link.isActive ? 'link mobile' : 'link')}
-                      to={mylink.to}
-                    >
-                      { mylink.name }
-                    </NavLink>
-                  </li>
-                );
-              })
-          }
-        <li><a className="sidenav-close" href="/" onClick={handleNav}><i className="medium right material-icons">clear_all</i></a></li>
+        {myLinks.map((mylink, index) => (
+          <li key={index}>
+            <NavLink
+              className="link"
+              activeClassName="mobile"
+              to={mylink.to}
+            >
+              {mylink.name}
+            </NavLink>
+          </li>
+        ))}
+
+        {/* Add Cancel Button */}
       </ul>
-      <a href="/" data-target="slide-out" className="sidenav-trigger" onClick={handleNav}><i className="material-icons"><i className="material-icons">menu</i></i></a>
+      <a href="#!" data-target="slide-out" className="sidenav-trigger" onClick={handleNav}><i className="material-icons"><img src="../images/menu1.png" alt="LinkedIn" /></i></a>
     </>
   );
 };
