4ba48aa0293b6cf50a292406e952767e48b9aae5
MICHAELKITH <michaelkithinji27@gmail.com>
2024-03-05 22:22:10 +0300

    Update the projects

--- dataItems.js
+++ dataItems.js
@@ -1,20 +1,4 @@
 const projs = [
-  {
-    title: 'Budget App ',
-    info: 'A budget management app built with Rails and Devise gem, with a clean and user-friendly interface designed with CSS and HTML. Users can create an account, log in, and manage their expenses and income. The app also features a dashboard displaying users current budget status, including charts and graphs that show the breakdown of their expense',
-    tech: ['HTML', 'CSS', 'Rails', 'Ruby', 'postgresql', 'Materialize'],
-    image: './images/bud1.png',
-    buttons: [
-      [
-        'https://mike-bpc1.onrender.com/',
-        'https://github.com/MICHAELKITH/budget_app.git',
-      ],
-      [
-        './desktop-images/Live_btn.png',
-        './desktop-images/Source_Btn.png',
-      ],
-    ],
-  },
 
   {
     title: 'Figma ',
@@ -28,22 +12,6 @@ const projs = [
       ],
       [
         './desktop-images/Live_btn.png',
-      ],
-    ],
-  },
-  {
-    title: 'To do ',
-    info: 'Easy, free, and simple-to-use HTML, CSS, and JavaScript project that may be used to schedule your daily activities.',
-    tech: ['HTML', 'CSS', 'JavaScript'],
-    image: './images/todo-1.png',
-    buttons: [
-      [
-        'https://michaelkith.github.io/To-do/',
-        'https://github.com/MICHAELKITH/To-do.git',
-      ],
-      [
-        './desktop-images/Live_btn.png',
-        './desktop-images/Source_Btn.png',
       ],
     ],
   },
--- miniData.js
+++ miniData.js
@@ -1,21 +1,5 @@
 const projs = [
   {
-    title: 'Budget-App',
-    info: 'A budget management app built with Rails and Devise gem, with a clean and user-friendly interface designed with CSS and HTML. Users can create an account, log in, and manage their expenses and income. The app also features a dashboard displaying users current budget status, including charts and graphs that show the breakdown of their expense',
-    tech: ['HTML', 'CSS', 'Rails', 'Ruby', 'postgresql', 'Materialize'],
-    image: './images/bud1.png',
-    buttons: [
-      [
-        'https://mike-bpc1.onrender.com/users/sign_up',
-        'https://github.com/MICHAELKITH/budget_app.git',
-      ],
-      [
-        './desktop-images/Live_btn.png',
-        './desktop-images/Source_Btn.png',
-      ],
-    ],
-  },
-  {
     title: 'Finance App',
     info: '[Finance APP ] is a react app that displays a list of stocks and services; when a user clicks on a stock, the app displays a list of companies that can be filtered using the search bar; each firm has its own photo, description, and details; and the app is free to use.',
     tech: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
@@ -24,6 +8,22 @@ const projs = [
       [
         'https://test-branch--deluxe-puffpuff-cd2d13.netlify.app/',
         'https://github.com/MICHAELKITH/Finance_app.git',
+      ],
+      [
+        './desktop-images/Live_btn.png',
+        './desktop-images/Source_Btn.png',
+      ],
+    ],
+  },
+  {
+    title: 'To do ',
+    info: 'Easy, free, and simple-to-use HTML, CSS, and JavaScript project that may be used to schedule your daily activities.',
+    tech: ['HTML', 'CSS', 'JavaScript'],
+    image: './images/todo-1.png',
+    buttons: [
+      [
+        'https://michaelkith.github.io/To-do/',
+        'https://github.com/MICHAELKITH/To-do.git',
       ],
       [
         './desktop-images/Live_btn.png',
